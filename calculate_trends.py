b0e23500d35dcd26b065105bfcef5551796fa394
Christian Poppe <c.poppe@fz-juelich.de>
2025-03-03 10:15:53 +0100

    add the possibility for different mk tests

--- calculate_trends.py
+++ calculate_trends.py
@@ -4,21 +4,10 @@ from timeit import default_timer as timer
 
 from blitzcomp.parallel import pixel_wise
 import data_config
+from argparse import ArgumentParser
 
-
-
-## Settings
-# The data that you specified in data_config.py
-data = 'SXI_183D_EU3_8daily' 
-variables = ['SXI_SM_r']
-outfile = 'out.nc'
-units = {'p': 'dimensionless',
-         'slope': 'm^3 m^-3 8d^-1'}
-
-
-if __name__ == '__main__':
-
-    def mannkendall(arrays: dict[str, np.ndarray]):
+def mannkendall(arrays: dict[str, np.ndarray],
+                test: str = 'original_test'):
 
         dict_out = {}
 
@@ -32,8 +21,10 @@ if __name__ == '__main__':
                 continue
             
             if array.ndim == 1:
+
+                func = getattr(pymk, test)
             
-                result = pymk.original_test(array)
+                result = func(array)
     
             else: 
             
@@ -44,23 +35,57 @@ if __name__ == '__main__':
     
         return dict_out
 
+
+if __name__ == '__main__':
+    
+    parser = ArgumentParser()
+    
+    parser.add_argument('--data', 
+                        '-d', 
+                        help = 'name of data dictionary', 
+                        type = str)
+
+    parser.add_argument('--variables', 
+                        '-v', 
+                        help = 'list of variable names',
+                        nargs ='*', 
+                        type = str)
+    
+    parser.add_argument('--outfile', 
+                        '-o', 
+                        help = 'path of the output file', 
+                        type = str)
+    
+    parser.add_argument('--test',
+                        '-t',
+                        help = 'mann kendall test type',
+                        type = str,
+                        default = 'original_test')
+    
+    args = parser.parse_args()
+    
+    units = {'p': 'dimensionless',
+             'slope': 'm^3 m^-3 8d^-1'}
+
     start = timer()
 
-    data_dict = getattr(data_config, data)
+    data_dict = getattr(data_config, args.data)
 
-    vars_out = [f'{v}_slope' for v in variables] + \
-               [f'{v}_p' for v in variables]
+    vars_out = [f'{v}_slope' for v in args.variables] + \
+               [f'{v}_p' for v in args.variables]
     
-    units_out = {v: units[v.split('_')[-1]] for v in vars_out}
+    units_out = {v: units[v.split('_')[-1]] 
+                 for v in vars_out}
 
     pixel_wise(mannkendall,
-               variables = variables,
+               variables = args.variables,
                data = data_dict,
                variables_out = vars_out,
                units = units_out,
-               file_out = outfile,
+               file_out = args.outfile,
                return_shape = 0,
-               return_dims = [])
+               return_dims = [],
+               test = args.test)
 
     end = timer()
 
--- submit.sh
+++ submit.sh
@@ -3,8 +3,33 @@
 #SBATCH --account=jibg31
 #SBATCH --ntasks=256
 #SBATCH --mem=512000
-#SBATCH --time=10:00
+#SBATCH --time=4:00:00
 
-source /p/scratch/cjibg31/jibg3105/projects/venvs/crusty/activate.sh
+# Environment
+source /p/scratch/cjibg31/jibg3105/projects/venvs/test_crusty/activate.sh
 
-srun -n 256 python calculate_trends.py
+# Data
+data=clm5_detect_control
+outfile=CLM5_DETECT_production_control_trends.nc
+variables=(
+    NEE
+    GPP
+    TLAI
+    TOTECOSYSC
+    TOTSOMC
+    TOTVEGC
+    LEAFC
+    QFLX_EVAP_TOT
+    QFLX_EVAP_VEG
+    ZWT
+)
+
+# https://pypi.org/project/pymannkendall/
+test=seasonal_test
+
+# run parallel
+srun -n 256 \
+    python calculate_trends.py \
+        --data $data \
+        --variables ${variables[@]} \
+        --outfile $outfile
